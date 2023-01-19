1a131dc97889f035e86b8465876694f30121ba2e
Nicholas Siciliano <nsicilia@oswego.edu>
2023-01-18 21:06:21 -0500

    Tabview Baseline

--- TestingLNPopUP/ContentView.swift
+++ TestingLNPopUP/ContentView.swift
@@ -9,16 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TabView {
+                FirstView()
+                    .tabItem {
+                        Image(systemName: "1.circle")
+                        Text("First Tab")
+                    }
+                SecondView()
+                    .tabItem {
+                        Image(systemName: "2.circle")
+                        Text("Second Tab")
+                    }
+                ThirdView()
+                    .tabItem {
+                        Image(systemName: "3.circle")
+                        Text("Third Tab")
+                    }
+            }
         }
-        
     }
-}
 
+    struct FirstView: View {
+        var body: some View {
+            Text("This is the first tab.")
+        }
+    }
+
+    struct SecondView: View {
+        var body: some View {
+            Text("This is the second tab.")
+        }
+    }
+
+    struct ThirdView: View {
+        var body: some View {
+            Text("This is the third tab.")
+        }
+    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
