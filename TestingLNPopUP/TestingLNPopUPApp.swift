e4c52430acf5fdc9bfbdc3ad17a2f8a5a1b33b68
Nicholas Siciliano <nsicilia@oswego.edu>
2023-01-18 20:55:56 -0500

    Initial Commit

---
+++ TestingLNPopUP/TestingLNPopUPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingLNPopUPApp.swift
+//  TestingLNPopUP
+//
+//  Created by Nicholas Siciliano-Salazar  on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingLNPopUPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
